70a7543d0352d64b7bda88fdbd388ba99aaf4436
Joshua <joshuanazareth97@gmail.com>
2018-10-16 11:52:02 +0530

    Added loop to build list of event details, temp code to generate CSV file

--- bms_scraper/app.py
+++ bms_scraper/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import urllib.request
 from bs4 import BeautifulSoup as bs
 import datetime
+import csv
 
 app = Flask(__name__)
 
@@ -17,7 +18,8 @@ def generate_csv():
     date_range = []
     date_filter = request.form.get("date")
     curr_day = datetime.datetime.now().weekday()
-    print(curr_day)
+
+    #creating list
     if date_filter == "today":
         date_range.append(datetime.datetime.now())
     elif date_filter == "tomorrow":
@@ -32,11 +34,10 @@ def generate_csv():
                 datetime.datetime.now()+datetime.timedelta(days=6-curr_day)]
         #handling edge case where this weekend filter chosen on saturday
         elif curr_day == 5:
-            date_range = [datetime.datetime.now().strftime("%Y%m%d"),
+            date_range = [datetime.datetime.now(),
                 datetime.datetime.now()+datetime.timedelta(days=1)]
         elif curr_day == 6:
             date_range.append(datetime.datetime.now())
-    print(date_range)
 
     #NOTE: hardcoded mumbai as default city
     with urllib.request.urlopen("https://in.bookmyshow.com/{}/events".format(request.form.get("city","mumbai"))) as bms:
@@ -45,21 +46,27 @@ def generate_csv():
     webpage_parsed = bs(webpage_html, "html.parser")
 
     event_tags = webpage_parsed.find_all(lambda x: str(x.name) == "aside" and str(x.get("id")).startswith("eventCards"))
+
+    #building final list of events
+    filtered_events = [["Title","Price","Date"]]
     for event in event_tags:
         #building list of datetime objects
         date_strings = event["data-datecode-filter"].split('|')
         if date_strings[-1] is "": date_strings = date_strings[:-1] #removes last element of date list (usually empty string)
         dates = [datetime.datetime.strptime(string,"%Y%m%d").date() for string in date_strings]
-        # print(dates)
-        if date_range and all(day.date() not in dates for day in date_range):
-            # print("Skipping")
-            continue
 
+        #Skip current event if filter present and it doesn't meet criteria
+        if date_range and all(day.date() not in dates for day in date_range): continue
+
+        #get title and price
         title = event.find("h4").string
         price = event["data-price-filter"]
-        print(title, price, dates[0])
-        # print(title,price)
-        # print(dates_list)
-        # # print(event.a.get("onclick"))
-        # print()
+
+        #append title, price and formatted datetime to list if filter criteria matched
+        filtered_events.append([title,price,dates[0].strftime("%d-%b-%Y")])
+
+    with open('events.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(filtered_events)
+
     return "Success"
